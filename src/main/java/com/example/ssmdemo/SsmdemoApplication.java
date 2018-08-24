3223f5d342e0478d199ee938921ddab1bd738650
你的用户名 <1440864716@qq.com>
2018-08-24 15:28:04 +0800

    日志管理

--- src/main/java/com/example/ssmdemo/SsmdemoApplication.java
+++ src/main/java/com/example/ssmdemo/SsmdemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.ssmdemo;
 
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,5 +14,13 @@ public class SsmdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SsmdemoApplication.class, args);
+    }
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void test() throws Exception {
+        logger.info("输出info");
+        logger.debug("输出debug");
+        logger.error("输出error");
     }
 }
